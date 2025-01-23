97b8f46a72fc39fa5f3de65a3f668c158cc50e1d
Febriansyah <febriansyah.febriansyah@diamond.co.id>
2025-01-23 11:11:23 +0700

    add row

--- routes/logs.js
+++ routes/logs.js
@@ -31,6 +31,52 @@ router.post('/add', protect, async (req, res) => {
     }
 });
 
+// Route to add a log (a stop)
+router.post('/add-multiple', protect, async (req, res) => {
+    const { clientNames, startTimes, endTimes } = req.body;
+    const user = req.user;
+
+    try {
+        if (clientNames.length == 0) {
+            return res.status(400).json({ message: 'All fields are required.' });
+        }
+        if (startTimes.length == 0) {
+            return res.status(400).json({ message: 'All fields are required.' });
+        }
+        if (endTimes.length == 0) {
+            return res.status(400).json({ message: 'All fields are required.' });
+        }
+        if (!user) {
+            return res.status(400).json({ message: 'User not authenticated' });
+        }
+
+        var logs = [];
+        for (let i = 0; i < clientNames.length; i++) {
+            const clientName = clientNames[i];
+            const startTime = startTimes[i];
+            const endTime = endTimes[i];
+            logs.push({
+                username: user.username,
+                clientName: clientName,
+                startTime: startTime,
+                endTime: endTime
+            })
+        }
+
+        Log.insertMany(logs)
+            .then(() => {
+                res.status(201).json({ message: 'Log added successfully', log: logs });
+            })
+            .catch((err) => {
+                console.log(err)
+                res.status(500).json({ message: 'Error saving log' });
+            });
+    } catch (error) {
+        console.error('Error adding log:', error);
+        res.status(500).json({ message: 'Error saving log' });
+    }
+});
+
 // Route to get logs - handles both admin and regular users
 router.get('/view', protect, async (req, res) => {
     const user = req.user;
